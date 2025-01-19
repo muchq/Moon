048f85858f1fe30468ec744815f6ab86939f87c9
Andy Aylward <aaylward@gmail.com>
2025-01-19 15:03:01 -0500

    add PeekRequest

--- cpp/golf_grpc/server/golf_grpc_service.cc
+++ cpp/golf_grpc/server/golf_grpc_service.cc
@@ -6,6 +6,7 @@
 #include "cpp/futility/status/status.h"
 #include "protos/golf_grpc/golf.pb.h"
 
+using namespace ::golf_grpc;
 using futility::status::AbseilToGrpc;
 using grpc::ServerContext;
 using grpc::Status;
@@ -13,15 +14,14 @@ using std::string;
 
 GolfServiceImpl::GolfServiceImpl(golf::GameManager gm) : gm_(std::move(gm)) {}
 
-Status GolfServiceImpl::RegisterUser(ServerContext* context,
-                                     const golf_grpc::RegisterUserRequest* request,
-                                     golf_grpc::RegisterUserResponse* response) {
+Status GolfServiceImpl::RegisterUser(ServerContext* context, const RegisterUserRequest* request,
+                                     RegisterUserResponse* response) {
   const auto status_or_username = gm_.registerUser(request->user_id());
   return AbseilToGrpc(status_or_username.status());
 };
 
-Status GolfServiceImpl::NewGame(ServerContext* context, const golf_grpc::NewGameRequest* request,
-                                golf_grpc::NewGameResponse* response) {
+Status GolfServiceImpl::NewGame(ServerContext* context, const NewGameRequest* request,
+                                NewGameResponse* response) {
   auto status_or_game_state = gm_.newGame(request->user_id(), request->number_of_players());
   if (!status_or_game_state.ok()) {
     return AbseilToGrpc(status_or_game_state.status());
@@ -33,36 +33,41 @@ Status GolfServiceImpl::NewGame(ServerContext* context, const golf_grpc::NewGame
   return Status::OK;
 };
 
-Status GolfServiceImpl::Peek(ServerContext* context, const golf_grpc::PeekRequest* request,
-                             golf_grpc::PeekResponse* response) {
+Status GolfServiceImpl::Peek(ServerContext* context, const PeekRequest* request,
+                             PeekResponse* response) {
+  auto status_or_game_state = gm_.peekAtDrawPile(request->game_id(), request->user_id());
+  if (!status_or_game_state.ok()) {
+    return AbseilToGrpc(status_or_game_state.status());
+  }
+
+  auto game_state = status_or_game_state.value();
+
+  HydrateResponseGameState(request->user_id(), response->mutable_game_state(), game_state);
   return Status::OK;
 };
 
-Status GolfServiceImpl::DiscardDraw(ServerContext* context,
-                                    const golf_grpc::DiscardDrawRequest* request,
-                                    golf_grpc::DiscardDrawResponse* response) {
+Status GolfServiceImpl::DiscardDraw(ServerContext* context, const DiscardDrawRequest* request,
+                                    DiscardDrawResponse* response) {
   return Status::OK;
 };
 
-Status GolfServiceImpl::SwapForDraw(ServerContext* context,
-                                    const golf_grpc::SwapForDrawRequest* request,
-                                    golf_grpc::SwapForDrawResponse* response) {
+Status GolfServiceImpl::SwapForDraw(ServerContext* context, const SwapForDrawRequest* request,
+                                    SwapForDrawResponse* response) {
   return Status::OK;
 };
 
-Status GolfServiceImpl::SwapForDiscard(ServerContext* context,
-                                       const golf_grpc::SwapForDiscardRequest* request,
-                                       golf_grpc::SwapForDiscardResponse* response) {
+Status GolfServiceImpl::SwapForDiscard(ServerContext* context, const SwapForDiscardRequest* request,
+                                       SwapForDiscardResponse* response) {
   return Status::OK;
 };
 
-Status GolfServiceImpl::Knock(ServerContext* context, const golf_grpc::KnockRequest* request,
-                              golf_grpc::KnockResponse* response) {
+Status GolfServiceImpl::Knock(ServerContext* context, const KnockRequest* request,
+                              KnockResponse* response) {
   return Status::OK;
 };
 
 void GolfServiceImpl::HydrateResponseGameState(const string& current_user_id,
-                                               golf_grpc::GameState* response_state,
+                                               GameState* response_state,
                                                const golf::GameStatePtr game_state) {
   response_state->set_all_here(game_state->allPlayersPresent());
   response_state->set_discard_size(game_state->getDiscardPile().size());
